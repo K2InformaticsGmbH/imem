e7fdc7a4897b92e51b8541893f713b86c7632328
Bikram Chatterjee <bikram@mspsbstun4.(none)>
2012-07-25 12:08:15 +0200

    Updated Start Script

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-if [ $# -eq 0 ]
-then
-    sname=imem
-else
-    sname=$1
-fi
-erl -pa ebin -boot start_sasl -config ${sname}_log -name ${sname}@WKS009.k2informatics.ch -env ERL_MAX_ETS_TABLES 10000 -mnesia schema_location ram dc_dump_limit 40 -setcookie ErlangSolutions -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020 -eval "apply(application, start, [sub_info])"
+
+erl -name imem@mspsbstun4.local -pa ebin -setcookie imem -eval "apply(net_adm, ping, ['cm@mspsbstun2.local'])" -s imem start -imem start_monitor true -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020 -env ERL_MAX_ETS_TABLES 10000
