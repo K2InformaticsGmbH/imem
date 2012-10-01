367edf627d6b45b6809fb3f2265688eb2fcebd12
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2012-10-01 13:46:58 +0200

    use relative ebin in erls.cmd

--- erls.cmd
+++ erls.cmd
@@ -1,4 +1,4 @@
-set Pa=D:\Work\Git\imem\ebin
+set Pa=ebin
 set Opts= -setcookie imem -env ERL_MAX_ETS_TABLES 10000 -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020 -eval "apply(net_adm, ping, ['CM@127.0.0.1'])" -s imem start -imem start_monitor true
 
 start werl.exe -name CM@127.0.0.1 -pa %Pa% -setcookie imem -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020
