a7133c4e26d85ea431c63d693bd51aa9cd6dbb18
Bikram Chatterjee <razorpeak@gmail.com>
2012-11-09 13:33:58 +0100

    updated clustering script

--- erls.cmd
+++ erls.cmd
@@ -1,9 +1,24 @@
+@echo off
 set Pa=ebin
-set Opts= -pa deps\sqlparse\ebin -setcookie imem -env ERL_MAX_ETS_TABLES 10000 -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020 -eval "apply(net_adm, ping, ['CM@127.0.0.1'])" -s imem start -imem start_monitor true
+set argC=0
+for %%x in (%*) do Set /A argC+=1
 
-start werl.exe -name CM@127.0.0.1 -pa %Pa% -setcookie imem -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020
+set cmNode=%1
+echo "%argC%"
+if %argC% EQU 2 (
+    set cmNode=%2
+    echo "CM on %cmNode%"
+    if %1==%2 (start werl.exe -name CM@%2 -pa %Pa% -setcookie imem -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020)
+) ELSE (
+    echo "Starting CM on same machine"
+    start werl.exe -name CM@%1 -pa %Pa% -setcookie imem -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020
+)
 
-start werl.exe -name A@127.0.0.1 -pa %Pa% %Opts% -imem node_type disc
-::start werl.exe -name B@127.0.0.1 -pa %Pa% %Opts% -imem node_type ram
-::start werl.exe -name C@127.0.0.1 -pa %Pa% %Opts% -imem node_type ram
-::start werl.exe -name D@127.0.0.1 -pa %Pa% %Opts% -imem node_type ram
+echo "%cmNode%"
+
+set Opts= -pa deps\sqlparse\ebin -setcookie imem -env ERL_MAX_ETS_TABLES 10000 -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020 -eval "apply(net_adm, ping, ['CM@%cmNode%'])" -s imem start -imem start_monitor true
+
+start werl.exe -name A@%1 -pa %Pa% %Opts% -imem node_type disc
+::start werl.exe -name B@%1 -pa %Pa% %Opts% -imem node_type ram
+::start werl.exe -name C@%1 -pa %Pa% %Opts% -imem node_type ram
+::start werl.exe -name D@%1 -pa %Pa% %Opts% -imem node_type ram
