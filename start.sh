6bed2fa4322ae9defa752f38caf1db0deab016bf
Bikram Chatterjee <razorpeak@gmail.com>
2012-11-15 21:36:32 +0100

    linux script for start imem cluster

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-erl -name imem@mspsbstun4.local -pa ebin -setcookie imem -eval "apply(net_adm, ping, ['cm@mspsbstun2.local'])" -s imem start -imem start_monitor true -kernel inet_dist_listen_min 9000 inet_dist_listen_max 9020 -env ERL_MAX_ETS_TABLES 10000
+bash erls.sh 127.0.0.1 127.0.0.1
