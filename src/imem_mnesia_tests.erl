785b9c78ecb3209b3c1ba3c8c2a626b4c4f7b7eb
Bikram Chatterjee <razorpeak@gmail.com>
2013-01-14 10:47:19 +0100

    mnesia async tests

--- src/imem_mnesia_tests.erl
+++ src/imem_mnesia_tests.erl
@@ -1,26 +1,26 @@
 -module(imem_mnesia_tests).
 
--include("imem_if.hrl").
+%% Application callbacks
+-compile(export_all).
+
 -include_lib("eunit/include/eunit.hrl").
 
--define(ROWCOUNT, 1000).
--define(THREAD_A_DELAY, 200).
--define(THREAD_B_DELAY, 200).
--define(THREAD_A_CHUNK, 10).
--define(THREAD_B_CHUNK, 20).
-
+-define(ROWCOUNT, 100).
+-define(THREAD_A_DELAY, 2000).
+-define(THREAD_B_DELAY, 2000).
+-define(THREAD_A_CHUNK, 5).
+-define(THREAD_B_CHUNK, 10).
 -define(TEST_TIMEOUT, 1000000).
-
--define(TABLE, imem_table_123).
 
 % EUnit tests --
 setup() ->
-    ?imem_test_setup(),
-    {atomic, ok} = mnesia:create_table(?TABLE, [{attributes, [col1, col2, col3]}]).
+    mnesia:start(),
+    {atomic, ok} = mnesia:create_table(table, [{attributes, [col1, col2, col3]}]),
+    ok.
 
 teardown(_) ->
-    catch imem_if:drop_table(?TABLE),
-    application:stop(imem).
+    mnesia:delete_table(table),
+    mnesia:stop().
 
 imem_mnesia_test_() ->
     {timeout, ?TEST_TIMEOUT, {
@@ -28,32 +28,44 @@ imem_mnesia_test_() ->
         fun setup/0,
         fun teardown/1,
         {with, [
-                fun concurent_test/1
+                fun run_test_eunit/1
         ]}
         }
     }.
 
-concurent_test(_) ->
+run_test_eunit(_) ->
+    io:format(user, "EUNIT ---~n", []),
+    run_test_core().
+
+run_test_core() ->
     async_insert(),
-    timer:sleep(?THREAD_A_DELAY),
+    timer:sleep(?THREAD_A_DELAY div 2),
     recv_async("_A_", ?THREAD_A_CHUNK, ?THREAD_A_DELAY),
-    TotalDelay = round(?ROWCOUNT / ?THREAD_A_CHUNK * ?THREAD_A_DELAY + ?ROWCOUNT / ?THREAD_B_CHUNK * ?THREAD_B_DELAY + ?THREAD_A_DELAY),
-%    TotalDelay = ?TEST_TIMEOUT div 400,
-    io:format(user, "waiting... ~p~n", [TotalDelay]),
+    recv_async("_B_", ?THREAD_B_CHUNK, ?THREAD_B_DELAY),
+    TotalDelay = round(?ROWCOUNT / ?THREAD_A_CHUNK * ?THREAD_A_DELAY + ?ROWCOUNT / ?THREAD_B_CHUNK * ?THREAD_B_DELAY),
+    io:format("waiting... ~p~n", [TotalDelay]),
     timer:sleep(TotalDelay).
+
+run_console() ->
+    io:format(user, "CONSOLE ---~n", []),
+    mnesia:start(),
+    {atomic, ok} = mnesia:create_table(table, [{attributes, [col1, col2, col3]}]),
+    run_test_core(),
+    mnesia:delete_table(table),
+    mnesia:stop().
 
 async_insert() ->
     F = fun
             (_, 0) -> ok;
             (F, R) ->
+                timer:sleep(?THREAD_B_DELAY div 20),
                 mnesia:transaction(fun() ->
                     io:format(user, "insert ~p~n", [R]),
-                    mnesia:write({?TABLE, R, R+1, R+2})
+                    mnesia:write({table, R, R+1, R+2})
                 end),
-%                timer:sleep(?THREAD_B_DELAY div 200),
                 F(F,R-1)
     end,
-    spawn(fun() -> F(F, ?ROWCOUNT) end).
+    spawn(fun() -> F(F, ?ROWCOUNT div 3) end).
 
 recv_async(Title, Limit, Delay) ->
     F0 = fun() ->
@@ -62,9 +74,9 @@ recv_async(Title, Limit, Delay) ->
             Pid ! next,
             receive
                 eot ->
-                    io:format(user, "[~p] finished~n", [Title]);
+                    io:format("[~p] finished~n", [Title]);
                 {row, Row} ->
-                    io:format(user, "[~p] got rows ~p~n", [Title, length(Row)]),
+                    io:format("[~p] got rows ~p~n", [Title, length(Row)]),
                     timer:sleep(Delay),
                     F(F)
             end
@@ -78,10 +90,10 @@ start_trans(Pid, Limit) ->
     fun(F,Contd0) ->
         receive
             abort ->
-                io:format(user, "Abort~n", []);
+                io:format("Abort~n", []);
             next ->
                 case (case Contd0 of
-                      undefined -> mnesia:select(?TABLE, [{'$1', [], ['$_']}], Limit, read);
+                      undefined -> mnesia:select(table, [{'$1', [], ['$_']}], Limit, read);
                       Contd0 -> mnesia:select(Contd0)
                       end) of
                 {Rows, Contd1} ->
@@ -91,5 +103,4 @@ start_trans(Pid, Limit) ->
                 end
         end
     end,
-    spawn(mnesia, transaction, [F, [F,undefined]]).
-
+    spawn(mnesia, async_dirty, [F, [F,undefined]]).
