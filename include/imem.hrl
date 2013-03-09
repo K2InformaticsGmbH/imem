50ffec03e373974579a8f05586fcd82fb35b3764
Bikram Chatterjee <razorpeak@gmail.com>
2013-03-09 10:01:52 +0100

    added lager like timestamps to LOG macro

--- include/imem.hrl
+++ include/imem.hrl
@@ -1,1 +1,8 @@
--define(Log(__F,__A), io:format(user, "[_IMEM_] "++__F, __A)).
+-define(_H,  element(1,erlang:time())).
+-define(_M,  element(2,erlang:time())).
+-define(_S,  element(3,erlang:time())).
+-define(_MS, element(3, erlang:now()) div 1000 rem 1000).
+
+-define(_T, lists:flatten(io_lib:format("~2..0B:~2..0B:~2..0B.~3..0B", [?_H,?_M,?_S,?_MS]))).
+
+-define(Log(__F,__A), io:format(user, ?_T++" [_IMEM_] "++__F, __A)).
