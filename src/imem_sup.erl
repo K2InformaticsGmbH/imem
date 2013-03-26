7568838f1476164bf54887058b2509e0e56fbda1
Bikram Chatterjee <razorpeak@gmail.com>
2013-03-26 15:17:48 +0100

    test_writer start ondemand imem:start_test_writer/1

--- src/imem_sup.erl
+++ src/imem_sup.erl
@@ -24,7 +24,10 @@
 %% Internal exports
 %% --------------------------------------------------------------------
 
--export([start/0, start_in_shell/0, start_link/1, init/1]).
+-export([start/0
+        , start_in_shell/0
+        , start_link/1
+        , init/1]).
 
 %% --------------------------------------------------------------------
 %% Macros
@@ -42,16 +45,20 @@
 
 start() ->
     spawn(fun() ->
-        supervisor:start_link({local,?MODULE}, ?MODULE, _Arg = [])
+        {ok, _} = supervisor:start_link({local,?MODULE}, ?MODULE, _Arg = []),
+        [?Log("imem process ~p started pid ~p~n", [Mod, Pid]) || {Mod,Pid,_,_} <- supervisor:which_children(?MODULE)]
     end).
     
 start_in_shell() ->
-    {ok, Pid} = supervisor:start_link({local,?MODULE}, ?MODULE, _Arg = []),
-    unlink(Pid).
+    {ok, SupPid} = supervisor:start_link({local,?MODULE}, ?MODULE, _Arg = []),
+    [?Log("imem process ~p started pid ~p~n", [Mod, Pid]) || {Mod,Pid,_,_} <- supervisor:which_children(?MODULE)],
+    unlink(SupPid).
     
 start_link(Args) ->
     case Result=supervisor:start_link({local,?MODULE}, ?MODULE, Args) of
-        {ok,_} ->   ?Log("~p started ~p~n", [?MODULE, Result]);
+        {ok,_} ->
+            [?Log("imem process ~p started pid ~p~n", [Mod, Pid]) || {Mod,Pid,_,_} <- supervisor:which_children(?MODULE)],
+            ?Log("~p started ~p~n", [?MODULE, Result]);
         Error ->    ?Log("~p startup failed with ~p~n", [?MODULE, Error])
     end,
     Result.
@@ -66,16 +73,16 @@ start_link(Args) ->
 %%          {error, Reason}
 %% --------------------------------------------------------------------
 init(_StartArgs) ->
-    {ok, MnesiaTimeout} = application:get_env(mnesia_timeout),
+    {ok, ImemTimeout} = application:get_env(imem_timeout),
     {ok, SchemaName} = application:get_env(mnesia_schema_name),
-    ?Log("~p initializing with MnesiaTimeout ~p~n", [?MODULE, MnesiaTimeout]),
+    ?Log("~p initializing with ImemTimeout ~p~n", [?MODULE, ImemTimeout]),
     {ok, NodeType} = application:get_env(mnesia_node_type),
     {ok, Mod} = application:get_env(if_mod),
     {ok, IsSec} = application:get_env(if_sec),
     DefParams = [{schema_name, SchemaName}, {node_type, NodeType}, {if_mod, Mod}, {if_sec, IsSec}],
     {ok, Servers} = application:get_env(servers),
     Children = [
-        {SMod, {SMod, start_link, [DefParams ++ SParams]}, permanent, MnesiaTimeout, worker, [SMod]}
+        {SMod, {SMod, start_link, [DefParams ++ SParams]}, permanent, ImemTimeout, worker, [SMod]}
         || {_SName, {SMod, SParams}} <- Servers],
     {ok, {{one_for_one, 3, 10}, Children}}.
 
