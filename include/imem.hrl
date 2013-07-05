5cce912604838b4491ee524df6646f3719b051cd
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2013-07-05 10:05:18 +0200

    short timestamp for test logs

--- include/imem.hrl
+++ include/imem.hrl
@@ -4,9 +4,17 @@
 (fun() ->
     {_,_,__McS} = __Now = erlang:now(),
     {{__YYYY,__MM,__DD},{__H,__M,__S}} = calendar:now_to_local_time(__Now),
-    lists:flatten(io_lib:format("~2..0B.~2..0B.~4..0B ~2..0B:~2..0B:~2..0B.~6..0B", [__DD,__MM,__YYYY,__H,__M,__S,__McS rem 1000000]))
+    lists:flatten(io_lib:format("~2..0B.~2..0B.~4..0B ~2..0B:~2..0B:~2..0B.~6..0B", [__DD,__MM,__YYYY,__H,__M,__S,__McS]))
 end)()).
--define(Log(__F,__A), io:format(user, ?__T++" ["++?LOG_TAG++"] {~p, ~4..0B} "++__F, [?MODULE,?LINE]++__A)).
+
+-define(__ST,
+(fun() ->
+    {_,_,__McS} = __Now = erlang:now(),
+    {{__YYYY,__MM,__DD},{__H,__M,__S}} = calendar:now_to_local_time(__Now),
+    lists:flatten(io_lib:format("~2..0B:~2..0B:~2..0B.~6..0B", [__H,__M,__S,__McS]))
+end)()).
+
+-define(Log(__F,__A), io:format(user, ?__ST++" ["++?LOG_TAG++"] {~p, ~4..0B} "++__F, [?MODULE,?LINE]++__A)).
 
 -ifdef(LAGER).
 
