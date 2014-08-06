9473f001f6983e5189dd5d2e2ecb05ff1b5b1238
Pieterjan Montens <pieterjan@montens.net>
2014-08-06 14:04:52 +0200

    added proposed functionality to imem_index comments

--- src/imem_index.erl
+++ src/imem_index.erl
@@ -61,9 +61,13 @@
 %%
 %% It could also be used to provide search-term and/or auto-correction suggestions.
 %%
+%% Index SHOULD NOT normalize (accent fold, lowercase, ...). That should be left over 
+%% to higher level processes (this precludes the use of binary:match/2 for any matching,
+%% because case insensitivity can not be guaranteed. Twice as slow regexp will have to be
+%% used instead).
 %%
 %% Suggested implementation:
-%% ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+%% ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
 %% As a simple_one_for_one gen_server, so index queries can be non-blocking and resolved
 %% in parallel, while still being supervised.
 %%
@@ -72,4 +76,24 @@
 %% 
 %% Offered functions would abstract different modes of usage, through the use of an
 %% environment setting, constant or even global variable.
+%%
+%%
+%% Observations:
+%% ¯¯¯¯¯¯¯¯¯¯¯¯¯
+%% - imem_index should use imem_if primitives to access data
+%%
+%% Proposed functionality:
+%% ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+%% case insensitive search: 
+%%    - provide IndexId, input string, Limit
+%%    - output format: [{headmatch, HeadMatchString, HeadMatchResults},
+%%                    {anymatch, AnyMatchString, AnyMatchResults},
+%%                    {regexpmatch, RegexpMatchString, RegexpMatchResults}]
+%%
+%%      How it should work:
+%%       Should first execute headmatch. If enough results, other result "sets" will be empty.
+%%       If not enough results, do anymatch (basic binary_match)
+%%       If input string contains wildcards or regexp-like characters (?), convert to regexp pattern, and match. 
+%%          Other result "sets" will be empty.
+
 
