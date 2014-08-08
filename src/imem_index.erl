aa926393202b0fa932220622b70d67d2b1a0d01d
Pieterjan Montens <pieterjan@montens.net>
2014-08-08 11:42:21 +0200

    work in progress on binary accent folding and to_lower and to_higher, binary matching primiteves added

--- src/imem_index.erl
+++ src/imem_index.erl
@@ -5,7 +5,15 @@
 
 -include("imem_meta.hrl").
 
--export([ binstr_to_lcase_ascii/1
+-export([binstr_to_lcase_ascii/1,
+
+         binstr_accentfold/1,
+         binstr_to_lower/1,
+         binstr_to_higher/1,
+
+         binstr_match_anywhere/2,
+         binstr_match_sub/4,
+         binstr_precompile/1
 		]).
 
 binstr_to_lcase_ascii(<<"\"\"">>) -> <<>>; 
@@ -118,4 +126,112 @@ unicode_string_to_ascii(U) ->
 %%        -> do anymatch (basic binary_match inside string)
 
 
+%% @doc Supports accent folding for all alphabetical characters supported by ISO 8859-15
+%% ISO 8859-15 supports the following languages: Albanian, Basque, Breton,
+%% Catalan,  Danish,  Dutch,  English, Estonian, Faroese, Finnish, French,
+%% Frisian,  Galician,  German,  Greenlandic,  Icelandic,  Irish   Gaelic,
+%% Italian,  Latin,  Luxemburgish,  Norwegian, Portuguese, Rhaeto-Romanic,
+%% Scottish Gaelic, Spanish, and Swedish.
+-spec binstr_accentfold(binary()) -> binary().
+binstr_accentfold(Str) when is_binary(Str) ->
+   b_convert(Str,<<>>). 
+
+    b_convert(<<>>,A) -> A;
+    b_convert(<<195,C,R/binary>>,A) when C >= 128, C =<  133 -> % À Á Â Ã Ä Å
+        b_convert(R,<<A/binary,$A>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 136, C =<  139 -> % È É Ê Ë
+        b_convert(R,<<A/binary,$E>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 140, C =<  143 -> % Ì Í Î Ï 
+        b_convert(R,<<A/binary,$I>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 146, C =<  148; C =:= 152 -> % Ò Ó Ô Õ Ö Ø
+        b_convert(R,<<A/binary,$O>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 153, C =<  156 -> % Ù Ú Û Ü
+        b_convert(R,<<A/binary,$U>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 157, C =<  159 -> % Ý
+        b_convert(R,<<A/binary,$Y>>);   
+    b_convert(<<195,C,R/binary>>,A) when C =:= 135 -> % Ç
+        b_convert(R,<<A/binary,$C>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 145 -> 
+        b_convert(R,<<A/binary,$N>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 134 -> % Æ
+        b_convert(R,<<A/binary,$A,$E>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 160 -> % LATIN CAPITAL LETTER S WITH CARON
+        b_convert(R,<<A/binary,$S>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 158 -> % Þ -> TH
+        b_convert(R,<<A/binary,$T,$H>>); 
+    
+    b_convert(<<195,C,R/binary>>,A) when C >= 160, C =<  165 -> 
+        b_convert(R,<<A/binary,$a>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 168, C =<  171 -> 
+        b_convert(R,<<A/binary,$e>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 172, C =<  175 -> 
+        b_convert(R,<<A/binary,$i>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 178, C =<  180 -> 
+        b_convert(R,<<A/binary,$o>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 185, C =<  188 -> 
+        b_convert(R,<<A/binary,$u>>);   
+    b_convert(<<195,C,R/binary>>,A) when C >= 189, C =<  191 -> 
+        b_convert(R,<<A/binary,$y>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 167 -> 
+        b_convert(R,<<A/binary,$c>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 166 -> 
+        b_convert(R,<<A/binary,$a,$e>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 177 -> 
+        b_convert(R,<<A/binary,$n>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 161 -> % LATIN SMALL LETTER S WITH CARON
+        b_convert(R,<<A/binary,$s>>); 
+    b_convert(<<195,C,R/binary>>,A) when C =:= 159 -> % ß -> ss
+        b_convert(R,<<A/binary,$s,$s>>); 
+    
+    b_convert(<<H,R/binary>>,A) -> 
+        b_convert(R,<<A/binary,H>>).
+
+%% @doc lowercases all alphabetical characters supported by ISO 8859-15
+-spec binstr_to_lower(binary()) -> binary().
+binstr_to_lower(Str) when is_binary(Str) ->
+    b_lower(Str,<<>>).
+
+    b_lower(<<>>,A) -> A;
+    b_lower(<<195,C,R/binary>>,A) when C >= 128, C =<  158 -> 
+        NC = C + 32,
+        b_lower(R,<<A/binary,195,NC>>);
+    b_lower(<<C,R/binary>>,A) when C >=  65, C =< 90 -> 
+        NC = C + 32,
+        b_lower(R,<<A/binary,NC>>);
+    b_lower(<<H,R/binary>>,A) -> 
+        b_lower(R,<<A/binary,H>>).
+ 
+%% @doc uppercases all alphabetical characters supported by ISO 8859-15
+-spec binstr_to_higher(binary()) -> binary().
+binstr_to_higher(Str) when is_binary(Str) ->
+    b_higher(Str,<<>>).
+
+    b_higher(<<>>,A) -> A;
+    b_higher(<<195,C,R/binary>>,A) when C >= 160, C =<  191 -> 
+        NC = C - 32,
+        b_higher(R,<<A/binary,195,NC>>);
+    b_higher(<<C,R/binary>>,A) when C >=  97, C =< 122 -> 
+        NC = C - 32,
+        b_higher(R,<<A/binary,NC>>);
+    b_higher(<<H,R/binary>>,A) -> 
+        b_higher(R,<<A/binary,H>>).
+
+binstr_match_anywhere(Token,Target) when is_binary(Token); is_tuple(Token) ->
+    case binary:match(Target,Token) of
+        nomatch -> false;
+        {_,_}   -> true
+    end;
+binstr_match_anywhere(Token,Target) when is_list(Token) ->
+    binstr_match_anywhere(list_to_binary(Token),Target).
+
+binstr_match_sub(Token,Start,Length,Target) when is_binary(Token); is_tuple(Token) ->
+    case binary:match(Target,Token,[{scope, {Start,Length}}]) of
+        nomatch -> false;
+        {_,_}   -> true
+    end;
+binstr_match_sub(Token,Start,Length,Target) when is_list(Token) ->
+    binstr_match_sub(list_to_binary(Token),Start,Length,Target).
+
+binstr_precompile(Token) ->
+    binary:compile_pattern(Token).
 
