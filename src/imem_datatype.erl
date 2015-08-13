fb83298885c7fadb330be43a441cf143c7fa12cb
Shamis Shukoor <shamis657@gmail.com>
2015-08-13 15:45:38 +0200

    Fixed the eunit failure

--- src/imem_datatype.erl
+++ src/imem_datatype.erl
@@ -741,9 +741,9 @@ io_to_datetime([${|_]=Val) ->
                 {{Y,M,D},{Hh,Mm,Ss}}
     end;
 io_to_datetime(Val0) ->
-    Val = re:replace(re:replace(re:replace(Val0, "T", " ", [{return, list}]),
-                     "Z", "", [{return, list}]),"[.*][0-9]*$", "", [{return, list}]),
-
+    Val = re:replace(re:replace(Val0, "T", " ", [{return, list}]),
+                     "(\\.[0-9]*Z|Z)$", "", [{return, list}]),
+                     
     try
         case re:run(lists:sublist(Val,5),"[\/\.\-]+",[{capture,all,list}]) of
             {match,["."]} ->
