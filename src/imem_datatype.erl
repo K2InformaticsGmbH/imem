c8ae2183736e4cd4804de59aef755d800bcf01db
Bikram Chatterjee <razorpeak@gmail.com>
2015-09-21 15:06:47 +0200

    added comparison and bit wise operations as safe to compile

--- src/imem_datatype.erl
+++ src/imem_datatype.erl
@@ -10,7 +10,12 @@
 -define(emptyIo,<<>>).
 
 
--define(SAFE_ERLANG_FUNCTIONS,['+','-','*','/','++','--','abs','div','rem','min','max','float','now','date','element','size','bit_size','byte_size','binary_part','phash2','md5','throw']).
+-define(SAFE_ERLANG_FUNCTIONS,['==','/=','=<','>','>=','>','=:=','=/=',
+                               '+','-','*','/','++','--','bnot','band','bor',
+                               'bxor','bsl','bsr','abs','div','rem',
+                               'min','max','float','now','date','element',
+                               'size','bit_size','byte_size','binary_part',
+                               'phash2','md5','throw']).
 -define(UNSAFE_ERLANG_FUNCTIONS,['list_to_atom','binary_to_atom','list_to_pid','binary_to_term','is_pid','is_port','is_process_alive']).
 
 -define(ROWFUN_EXTENSIONS,[{<<"nodef">>,1}
