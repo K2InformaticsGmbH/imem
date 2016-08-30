f75ff615a109cd7ead3ae0a4789cd0666173fadc
Shamis Shukoor <shamis657@gmail.com>
2016-08-30 12:13:58 +0200

    Review change #99

--- src/imem_seco.erl
+++ src/imem_seco.erl
@@ -593,10 +593,10 @@ auth_step(SeCo, {smsott,Token}) ->
 auth_step(SeCo, {saml, Name}) ->
     #ddSeCo{skey=SKey, accountId=AccountId0, authFactors=AFs} = SeCo, % may not yet exist in ddSeco@
     case if_select_account_by_name(SKey, Name) of
-        {[#ddAccount{locked='true'}],true} ->
-            authenticate_fail(SeCo, "Account is locked. Contact a system administrator", true);
         {[#ddAccount{id=AccountId1}],true} when AccountId0==AccountId1; AccountId0==undefined ->
             auth_step_succeed(SeCo#ddSeCo{accountName=Name, accountId=AccountId1, authFactors=[saml|AFs]});
+        {[#ddAccount{locked='true'}],true} ->
+            authenticate_fail(SeCo, "Account is locked. Contact a system administrator", true);
         {[#ddAccount{}],true} -> 
             authenticate_fail(SeCo, "Account name conflict", true);
         {[],true} ->
