d4e4ed4341614872fff6c61d9d5a829f32a84b46
Agustin Cautin <acautin@gmail.com>
2016-11-24 15:57:47 +0100

    Modified imem_gen_metrics behaviour and imem_metrics to support asynchronous calls for metrics.

--- src/imem_gen_metrics.erl
+++ src/imem_gen_metrics.erl
@@ -5,7 +5,9 @@
 
 -behaviour(gen_server).
 
--export([get_metric/2]).
+-export([get_metric/2
+        ,get_metric/3
+        ,request_metric/4]).
 
 -export([start_link/1
         ,init/1
@@ -22,16 +24,31 @@
                ,system_time :: integer()
                ,system_state = normal :: atom()}).
 
+-define(DEFAULT_REQ_TIMEOUT, 10000).
+
 -callback init() -> {ok, term()} | {error, term()}.
--callback handle_metric_req(MetricKey :: term(), State :: term()) -> Result :: [map()].
+-callback handle_metric_req(MetricKey :: term(), ReplyFun :: fun(), State :: term()) -> NewState :: term().
+-callback terminate(Reason :: term(), State :: term()) -> ok.
 
 -spec start_link(atom()) -> {ok, pid()} | {error, term()}.
 start_link(Mod) ->
     gen_server:start_link({local, Mod}, ?MODULE, [Mod], []).
 
--spec get_metric(atom(), term()) -> [map()].
+-spec get_metric(atom(), term()) -> term() | {error, timeout}.
 get_metric(Mod, MetricKey) ->
-    gen_server:call(Mod, {get_metric, MetricKey}).
+    get_metric(Mod, MetricKey, ?DEFAULT_REQ_TIMEOUT).
+
+-spec get_metric(atom(), term(), integer()) -> term() | timeout.
+get_metric(Mod, MetricKey, Timeout) ->
+    ReqRef = erlang:make_ref(),
+    gen_server:cast(Mod, {request_metric, MetricKey, ReqRef, self()}),
+    receive {metric, ReqRef, Metric} -> Metric
+    after Timeout -> timeout
+    end.
+
+-spec request_metric(atom(), term(), term(), pid()) -> ok.
+request_metric(Mod, MetricKey, ReqRef, ReplyTo) ->
+    gen_server:cast(Mod, {request_metric, MetricKey, ReqRef, ReplyTo}).
 
 %% Gen server callback implementations.
 init([Mod]) ->
@@ -44,14 +61,39 @@ init([Mod]) ->
         {error, Reason} -> {stop, Reason}
     end.
 
-handle_call({get_metric, MetricKey}, _From, #state{mod = Mod, impl_state = ImplState, system_state = SysState} = State) ->
+handle_call(UnknownReq, _From, #state{mod = Mod} = State) ->
+    ?Error("~p implementing ~p pid ~p received unknown call ~p", [Mod, ?MODULE, self(), UnknownReq]),
+    {noreply, State}.
+
+handle_cast({request_metric, MetricKey, ReqRef, ReplyTo}, #state{} = State) ->
+    ReplyFun = build_reply_fun(ReqRef, ReplyTo),
+    NewState = internal_get_metric(MetricKey, ReplyFun, State),
+    {noreply, NewState};
+handle_cast(UnknownReq, #state{mod = Mod} = State) ->
+    ?Error("~p implementing ~p pid ~p received unknown cast ~p", [Mod, ?MODULE, self(), UnknownReq]),
+    {noreply, State}.
+
+handle_info(Message, State) ->
+    ?Error("~p doesn't message unexpected: ~p", [?MODULE, Message]),
+    {noreply, State}.
+
+terminate(Reason, #state{mod=Mod, impl_state=ImplState}) ->
+    Mod:terminate(Reason, ImplState).
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+%% Helper functions
+-spec internal_get_metric(term(), fun(), #state{}) -> #state{}.
+internal_get_metric(MetricKey, ReplyFun, #state{mod=Mod, impl_state=ImplState, system_state=SysState} = State) ->
     Time = os:system_time(micro_seconds),
     ElapsedSeconds = (Time - State#state.system_time) / 1000000,
     case {ElapsedSeconds < 1, SysState} of
         {true, normal} ->
-            {Metric, NewImplState} = Mod:handle_metric_req(MetricKey, ImplState),
-            {reply, Metric, State#state{impl_state = NewImplState}};
-        {true, _} -> {reply, SysState, State};
+            NewImplState = Mod:handle_metric_req(MetricKey, ReplyFun, ImplState),
+            State#state{impl_state = NewImplState};
+        {true, _} ->
+            ReplyFun(SysState),
+            State;
         {false, _} ->
             MaxReductions = ?GET_CONFIG(maxReductions,[Mod],100000000,"Max number of reductions per second before considering the system as overloaded."),
             MaxMemory = ?GET_CONFIG(maxMemory,[Mod],90,"Memory usage before considering the system as overloaded."),
@@ -60,25 +102,22 @@ handle_call({get_metric, MetricKey}, _From, #state{mod = Mod, impl_state = ImplS
             ReductionsRate = ElapsedReductions/ElapsedSeconds,
             {_, FreeMemory, TotalMemory} = imem:get_os_memory(),
             PctMemoryUsed = 100 - FreeMemory / TotalMemory * 100,
-            {{Metric, NewImplState}, NewSysState} = case {ReductionsRate > MaxReductions, PctMemoryUsed > MaxMemory} of
-                {true, _} -> {{cpu_overload, ImplState}, cpu_overload};
-                {_, true} -> {{memory_overload, ImplState}, memory_overload};
-                _ -> {Mod:handle_metric_req(MetricKey, ImplState), normal}
+            {NewImplState, NewSysState} = case {ReductionsRate > MaxReductions, PctMemoryUsed > MaxMemory} of
+                {true, _} ->
+                    ReplyFun(cpu_overload),
+                    {ImplState, cpu_overload};
+                {_, true} ->
+                    ReplyFun(memory_overload),
+                    {ImplState, memory_overload};
+                _ ->
+                    {Mod:handle_metric_req(MetricKey, ReplyFun, ImplState), normal}
             end,
-            {reply, Metric, State#state{impl_state = NewImplState
-                                       ,reductions = Reductions
-                                       ,system_time = Time
-                                       ,system_state = NewSysState}}
+            State#state{impl_state = NewImplState
+                       ,reductions = Reductions
+                       ,system_time = Time
+                       ,system_state = NewSysState}
     end.
 
-handle_cast(Unknown, State) ->
-    ?Error("~p implementing ~p pid ~p received unknown cast ~p", [?MODULE, self(), Unknown]),
-    {noreply, State}.
-
-handle_info(Message, State) ->
-    ?Error("~p doesn't message unexpected: ~p", [?MODULE, Message]),
-    {noreply, State}.
-
-terminate(_Reason, _State) -> ok.
-
-code_change(_OldVsn, State, _Extra) -> {ok, State}.
+-spec build_reply_fun(term(), pid()) -> fun().
+build_reply_fun(ReqRef, ReplyTo) ->
+    fun(Result) -> ReplyTo ! {metric, ReqRef, Result} end.
--- src/imem_metrics.erl
+++ src/imem_metrics.erl
@@ -6,32 +6,49 @@
 
 -export([start_link/1
         ,get_metric/1
+        ,get_metric/2
+        ,request_metric/3
         ]).
 
 -export([init/0
-        ,handle_metric_req/2
+        ,handle_metric_req/3
+        ,terminate/2
         ]).
 
 -spec start_link(list()) -> {ok, pid()} | {error, term()}.
 start_link(_) ->
     imem_gen_metrics:start_link(?MODULE).
 
--spec get_metric(atom()) -> [map()].
+-spec get_metric(term()) -> term().
 get_metric(MetricKey) ->
     imem_gen_metrics:get_metric(?MODULE, MetricKey).
 
+-spec get_metric(term(), integer()) -> term().
+get_metric(MetricKey, Timeout) ->
+    imem_gen_metrics:get_metric(?MODULE, MetricKey, Timeout).
+
+-spec request_metric(term(), term(), pid()) -> ok.
+request_metric(MetricKey, ReqRef, ReplyTo) ->
+    imem_gen_metrics:request_metric(?MODULE, MetricKey, ReqRef, ReplyTo).
 
 %% imem_gen_metrics callback
 init() -> {ok, undefined}.
 
-handle_metric_req(system_information, State) ->
+handle_metric_req(system_information, ReplyFun, State) ->
     {_, FreeMemory, TotalMemory} = imem:get_os_memory(),
-    {#{free_memory => FreeMemory,
-       total_memory => TotalMemory,
-       erlang_memory => erlang:memory(total),
-       process_count => erlang:system_info(process_count),
-       port_count => erlang:system_info(port_count), 
-       run_queue => erlang:statistics(run_queue)}, State};
-handle_metric_req(UnknownMetric, State) ->
+    Result = #{
+        free_memory => FreeMemory,
+        total_memory => TotalMemory,
+        erlang_memory => erlang:memory(total),
+        process_count => erlang:system_info(process_count),
+        port_count => erlang:system_info(port_count), 
+        run_queue => erlang:statistics(run_queue)
+    },
+    ReplyFun(Result),
+    State;
+handle_metric_req(UnknownMetric, ReplyFun, State) ->
     ?Error("Unknow metric requested ~p", [UnknownMetric]),
-    {undefined, State}.
+    ReplyFun(undefined),
+    State.
+
+terminate(_Reason, _State) -> ok.
