85a037cb77fbfd9aab2a687311d3d69fb7bcca13
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2017-02-06 16:51:27 +0100

    fix issue with multiple thousand separators in imem_datatype

--- src/imem_datatype.erl
+++ src/imem_datatype.erl
@@ -16,8 +16,8 @@
 
 -define(H(X), (hex(X)):16).
 
--define(THOUSAND_SEP,$').           
--define(USE_THOUSAND_SEP,true).     %% comment out if no thousands separation is wanted
+-define(THOUSAND_SEP,[$',$,]).      %% ' or , are ignored in integer strings (first one is used on output)     
+-define(USE_THOUSAND_SEP,true).     %% comment out if no thousands separation is wanted on output
 
 -define(BinaryMaxLen,250).          %% more represented by "..." suffix
 
@@ -549,7 +549,7 @@ io_to_integer(Val,Len,undefined) ->
 io_to_integer(Val,Len,Prec) when is_binary(Val) ->
     io_to_integer(binary_to_list(Val),Len,Prec);
 io_to_integer(Val,Len,Prec) ->
-    Value = try  list_to_integer(lists:delete(?THOUSAND_SEP,Val))
+    Value = try  list_to_integer(string:join(string:tokens(Val,?THOUSAND_SEP),[]))
         catch _:_ -> 
             try erlang:round(list_to_float(Val))
             catch _:_ ->  
@@ -949,7 +949,7 @@ io_to_decimal(Val,Len,undefined) ->
 io_to_decimal(Val,Len,0) ->         %% use fixed point arithmetic with implicit scaling factor
     io_to_integer(Val,Len,0);
 io_to_decimal(Val,Len,Prec) ->
-    case lists:delete(?THOUSAND_SEP,Val) of
+    case string:join(string:tokens(Val,?THOUSAND_SEP),[]) of
         [$-|Positive] -> Sign = [$-];
         Positive -> Sign = []
     end,
@@ -1334,7 +1334,7 @@ integer_to_io(Val) ->
 integer_filter($-,Acc) -> [$-|Acc];
 integer_filter(D,Acc) -> 
     case (length(Acc)+1) rem 4 of
-        0 ->    [D,?THOUSAND_SEP|Acc];
+        0 ->    [D,hd(?THOUSAND_SEP)|Acc];
         _ ->    [D|Acc]
     end.
 -else.
@@ -1699,6 +1699,9 @@ data_types(_) ->
         ?assertEqual(-200, io_to_db(Item,OldInteger,integer,100,0,Def,RW,<<"300-500">>)),
         ?assertEqual(12, io_to_db(Item,OldInteger,integer,20,0,Def,RW,<<"120/10.0">>)),
         ?assertEqual(12, io_to_db(Item,OldInteger,integer,20,0,Def,RW,<<"120/10.0001">>)),
+        ?assertEqual(123456789, io_to_db(Item,OldInteger,integer,20,0,Def,RW,<<"123'456'789">>)),
+        ?assertEqual(-123456789, io_to_db(Item,OldInteger,integer,20,0,Def,RW,<<"-123,456,789">>)),
+        ?assertException(throw, {ClEr,{"Data conversion format error",{0,{integer,<<"12'000/10.0">>}}}}, io_to_db(Item,OldInteger,integer,20,0,Def,RW,<<"12'000/10.0">>)),
         ?assertException(throw, {ClEr,{"Data conversion format error",{0,{integer,3,1,"1234"}}}}, io_to_db(Item,OldInteger,integer,Len,Prec,Def,RW,<<"1234">>)),
         ?assertException(throw, {ClEr,{"Data conversion format error",{0,{integer,<<"-">>}}}}, io_to_db(Item,OldInteger,integer,Len,Prec,Def,RW,<<"-">>)),
         ?assertEqual(default, io_to_db(Item,OldInteger,integer,Len,Prec,Def,RW,?emptyIo)),
