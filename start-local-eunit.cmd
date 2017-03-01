3e0a732f66fca8b3ee720fe7d198a7655a36926a
walter-weinmann <walter.weinmann@gmail.com>
2017-03-01 15:58:15 +0100

    Remove legacy scripts.

--- start-local-eunit.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set Opts= -pa _build\default\lib\imem\ebin -pa _build\default\lib\sqlparse\ebin -setcookie imem -env ERL_MAX_ETS_TABLES 10000
-set ImemOpts= -s imem start -imem mnesia_node_type disc
-start werl.exe %Opts% %ImemOpts%
