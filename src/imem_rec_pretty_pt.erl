be387f6d3758da543971214fe5ad4a053e3b35d7
Bikram Chatterjee <razorpeak@gmail.com>
2017-06-21 18:03:24 +0200

    no exports, light-weight forms traversal, full forms coverage

--- src/imem_rec_pretty_pt.erl
+++ src/imem_rec_pretty_pt.erl
@@ -5,9 +5,9 @@
 
 parse_transform(Forms, _Options) ->
     try
-        {Functions, Exports} =
+        Functions =
         lists:foldl(
-          fun({attribute,_,record,{Record,RFields}}, {Funcs, Exports}) ->
+          fun({attribute,_,record,{Record,RFields}}, Funcs) ->
                  FieldNames =
                  [case R of
                       {record_field,_,{atom,_,N}} -> N;
@@ -16,31 +16,15 @@ parse_transform(Forms, _Options) ->
                       {typed_record_field,{record_field,_,{atom,_,N},_},_} ->
                           N
                   end || R <- RFields],
-                 Fun = list_to_atom(atom_to_list(Record)++"_pretty"),
-                 {[rf(Record, Fun, FieldNames) | Funcs],
-                  [{attribute,1,export,[{Fun,1}]} | Exports]};
+                 RecFun = list_to_atom(atom_to_list(Record)++"_pretty"),
+                 Funcs#{RecFun => rf(Record, RecFun, FieldNames)};
              (_, Acc) -> Acc
-          end, {[], []}, Forms),
-        RecFuns = [F || {attribute,1,export,[{F,1}]} <- Exports],
+          end, #{}, Forms),
+        RecFuns = maps:keys(Functions),
         CalledRecFuns = calls(Forms, RecFuns),
-        {UsedFunctions, UsedExports} =
-        lists:unzip(
-          lists:filter(
-            fun({{function,1,F,1,_}, {attribute,1,export,[{F,1}]}}) ->
-                    case lists:member(F, CalledRecFuns) of
-                        true -> true;
-                        _ -> false
-                    end
-            end, lists:zip(Functions, Exports))),
-        io:format("Forms ~p~n", [Forms]),
-        io:format("UsedFunctions ~p~nUsedExports ~p~n",
-                  [UsedFunctions, UsedExports]),
-        case ins_exprts(UsedExports, Forms) of
-            Forms -> Forms;
-            Forms1 ->
-                [{eof,_} = EOF | Rest] = lists:reverse(Forms1),
-                lists:reverse([EOF|UsedFunctions]++Rest)
-        end
+        UsedFunctions = maps:values(maps:with(CalledRecFuns, Functions)),
+        [{eof,_} = EOF | Rest] = lists:reverse(Forms),
+        lists:reverse([EOF|UsedFunctions]++Rest)
     catch
         _:Error ->
             ?L("parse transform failed~n~p~n~p~n",
@@ -50,48 +34,16 @@ parse_transform(Forms, _Options) ->
 
 calls(Forms, RecFuns) -> calls(Forms, [], RecFuns).
 calls([], Acc, _RecFuns) -> lists:usort(Acc);
-calls([Head|Rest], Acc, RecFuns) ->
-    calls(Rest,
-          case filter_call(Head, RecFuns) of
-              skip -> Acc;
-              Calls when is_list(Calls) -> Calls ++ Acc;
-              Call -> [Call | Acc]
-          end, RecFuns).
-
-filter_call({function,_,_,_,FBody}, RecFuns) ->
-    calls(FBody, RecFuns);
-filter_call({clause,_,_,_,CBody}, RecFuns) ->
-    calls(CBody, RecFuns);
-filter_call({call,_,{atom,_,Fn},_}, RecFuns) ->
+calls({call,_,{atom,_,Fn},_}, Acc, RecFuns) ->
     case lists:member(Fn, RecFuns) of
-        true -> Fn;
-        _ -> skip
+        true -> [Fn | Acc];
+        _ -> Acc
     end;
-filter_call({call,_,{remote,_,_,_},CBody}, RecFuns) ->
-    calls(CBody, RecFuns);
-filter_call({call,_,{'fun',_,{clauses,Clauses}},CBody}, RecFuns) ->
-    calls(Clauses, RecFuns) ++ calls(CBody, RecFuns);
-filter_call({tuple,_,Tuple}, RecFuns) ->
-    calls(Tuple, RecFuns);
-filter_call({cons,_,Cons,CTail}, RecFuns) ->
-    calls([CTail, Cons], RecFuns);
-filter_call(_Other, _RecFuns) ->
-    %io:format("SKIP ~p~n", [_Other]),
-    skip.
-
-ins_exprts(Exports, [_|_] = Forms) ->
-    case lists:usort([lists:member(E, Forms) || E <- Exports]) of
-        [false] -> ins_exprts(Exports, {[], Forms});
-        _ -> Forms
-    end;
-ins_exprts([], {Heads,Tail}) -> lists:reverse(Heads)++Tail;
-ins_exprts(Exports, {Heads, []}) ->
-    [{attribute,_,file,_} = F, {attribute,_,module,_} = M | R] = lists:reverse(Heads),
-    [F,M|Exports++R];
-ins_exprts(Exports, {Heads, [{attribute,_,export,_} = E | Tail]}) ->
-    ins_exprts([], {[E | Exports] ++ Heads, Tail});
-ins_exprts(Exports, {Heads, [F | Tail]}) ->
-    ins_exprts(Exports, {[F | Heads], Tail}).
+calls([Head|Rest], Acc, RecFuns) ->
+    calls(Rest, calls(Head, Acc, RecFuns), RecFuns);
+calls(Tuple, Acc, RecFuns) when is_tuple(Tuple) ->
+    calls(tuple_to_list(Tuple), Acc, RecFuns);
+calls(_, Acc, _RecFuns) -> Acc.
 
 rf(Record, Fun, FieldNames) ->
     Fmt =
