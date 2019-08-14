563facba7f1563ae8990b63cf057c4a8694bc229
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2019-08-14 16:26:33 +0200

    single partition version ready for tests

--- src/imem_statement.erl
+++ src/imem_statement.erl
@@ -704,20 +704,20 @@ process_tail_row(R0,#state{isSec=IsSec,seco=SKey,fetchCtx=FetchCtx0,statement=St
                 Rem0 =< 1 ->
                     % ?LogDebug("send~n~p~n", [{RawRecords,true}]),
                     unsubscribe(Stmt),
-                    {{RawRecords,true}, State#state{fetchCtx=FetchCtx0#fetchCtx{status=done}}};
+                    {{self(),RawRecords,true}, State#state{fetchCtx=FetchCtx0#fetchCtx{status=done}}};
                 true ->
                     % ?LogDebug("send~n~p~n", [{RawRecords,tail}]),
-                    {{RawRecords,tail}, State#state{fetchCtx=FetchCtx0#fetchCtx{remaining=Rem0-1}}}
+                    {{self(),RawRecords,tail}, State#state{fetchCtx=FetchCtx0#fetchCtx{remaining=Rem0-1}}}
             end;
         {_,1,_FirstRN} ->    %% single table select, avoid join overhead but update RowNum meta field
             if
                 Rem0 =< 1 ->
                     % ?LogDebug("send~n~p~n", [{RawRecords,true}]),
                     unsubscribe(Stmt),
-                    {{RawRecords,true}, State#state{fetchCtx=FetchCtx0#fetchCtx{status=done}}};
+                    {{self(),RawRecords,true}, State#state{fetchCtx=FetchCtx0#fetchCtx{status=done}}};
                 true ->
                     % ?LogDebug("send~n~p~n", [{RawRecords,tail}]),
-                    {{RawRecords,tail}, State#state{fetchCtx=FetchCtx0#fetchCtx{rownum=RowNum+1,remaining=Rem0-1}}}
+                    {{self(),RawRecords,tail}, State#state{fetchCtx=FetchCtx0#fetchCtx{rownum=RowNum+1,remaining=Rem0-1}}}
             end;
         {_,_,undefined} ->        %% join raw result of main scan with remaining tables
             case join_rows(RawRecords, FetchCtx0, Stmt) of
@@ -728,10 +728,10 @@ process_tail_row(R0,#state{isSec=IsSec,seco=SKey,fetchCtx=FetchCtx0,statement=St
                         (Rem0 =< length(Result)) ->
                             % ?LogDebug("send~n~p~n", [{Result,true}]),
                             unsubscribe(Stmt),
-                            {{Result, true}, State#state{fetchCtx=FetchCtx0#fetchCtx{status=done}}};
+                            {{self(),Result,true}, State#state{fetchCtx=FetchCtx0#fetchCtx{status=done}}};
                         true ->
                             % ?LogDebug("send~n~p~n", [{Result,tail}]),
-                            {{Result, tail}, State#state{fetchCtx=FetchCtx0#fetchCtx{remaining=Rem0-length(Result)}}}
+                            {{self(),Result,tail}, State#state{fetchCtx=FetchCtx0#fetchCtx{remaining=Rem0-length(Result)}}}
                     end
             end;
         {_,_,_} ->        %% join raw result of main scan with remaining tables, correct RowNum
@@ -743,10 +743,10 @@ process_tail_row(R0,#state{isSec=IsSec,seco=SKey,fetchCtx=FetchCtx0,statement=St
                         (Rem0 =< length(JoinedRows)) ->
                             % ?LogDebug("send~n~p~n", [{Result,true}]),
                             unsubscribe(Stmt),
-                            {{update_row_num(MR2, RowNum, JoinedRows), true}, State#state{fetchCtx=FetchCtx0#fetchCtx{status=done}}};
+                            {{self(),update_row_num(MR2, RowNum, JoinedRows),true}, State#state{fetchCtx=FetchCtx0#fetchCtx{status=done}}};
                         true ->
                             % ?LogDebug("send~n~p~n", [{Result,tail}]),
-                            {{update_row_num(MR2, RowNum, JoinedRows), tail}, State#state{fetchCtx=FetchCtx0#fetchCtx{rownum=RowNum+length(JoinedRows),remaining=Rem0-length(JoinedRows)}}}
+                            {{self(),update_row_num(MR2, RowNum, JoinedRows),tail}, State#state{fetchCtx=FetchCtx0#fetchCtx{rownum=RowNum+length(JoinedRows),remaining=Rem0-length(JoinedRows)}}}
                     end
             end
     end.
@@ -1058,7 +1058,7 @@ generate_limit_check(Table, CurSize, MaxSize) when CurSize > MaxSize ->
 generate_limit_check(_,_,_) -> ok. 
 
 send_reply_to_client(SockOrPid, Result) ->
-    NewResult = {self(),Result},
+    NewResult = {self(), Result},
     imem_server:send_resp(NewResult, SockOrPid).
 
 update_prepare(IsSec, SKey, [{Schema,Table}|_], ColMap, ChangeList) ->
