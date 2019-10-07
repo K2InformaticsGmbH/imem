7a4b0148eff31f42c74ebcf6f3bc0620107cc38f
walter-weinmann <USR8fvB2fEZ4>
2019-10-07 15:00:34 +0200

    IP address 0.0.0.0 instead of 127.0.0.1.

--- src/imem_win32.erl
+++ src/imem_win32.erl
@@ -7,7 +7,7 @@
 init() ->
     case os:type() of
         {win32, nt} ->
-            erlang:load_nif(imem:priv_dir() ++ "/imem_win32", 0);
+            ok = erlang:load_nif(imem:priv_dir() ++ "/imem_win32", 0);
         _ -> ok
     end.
 
